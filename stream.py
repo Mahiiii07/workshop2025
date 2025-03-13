363b7ece4dc80c1e9c74fc5680e9151ec062b0f8
Mahiiii07 <gajjarmahi40@gmail.com>
2025-03-13 12:58:31 +0530

    Streamlit

---
+++ stream.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('My First Cloud App')
+st.write("A Simple Dataframes : ")
+df = pd.DataFrame(np.random.randn(10,2), columns = ['Col1','Col2'])
+st.dataframe(df)
